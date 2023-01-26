c9532638d91c79b42d3ba90357eaf27219bf7fac
ABHI5645 <51890402+ABHI5645@users.noreply.github.com>
2023-01-26 19:09:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 // complete the given function
 
 function palindrome(str){
-
+     let str2=str.split('').reverse().join('');
+	if(str===str2){
+		return true;
+	}
+	else{
+		return false;
+	}
 }
 module.exports = palindrome
